030b5893bfd8f66927728ef2f59c2c8d3db448ef
jjheras <jjheras@live.com>
2024-04-09 16:46:44 +0200

    incorporación de sumar, restar, multiplicar, dividir

--- suma.py
+++ suma.py
@@ -1,4 +1,15 @@
 def suma (a,b):
     return a+b
+def resta (a,b):
+    return a-b
+def multiplicacion (a,b):
+    return a*b
+def division(a,b):
+    if b == 0:
+        return "Error, no se puede dividir entre 0"
+    else:
+        return a/b
+
+
 if __name__ == "__main__":
     print(suma(5,3))
--- test_suma.py
+++ test_suma.py
@@ -1,11 +1,35 @@
 import unittest
-from suma import suma
+from suma import suma,resta,multiplicacion,division
 
 class TestSumar(unittest.TestCase):
     def test_suma(self):
         self.assertEqual(suma(3,2), 5)
         self.assertEqual(suma(-1,1), 0)
         self.assertEqual(suma(-1,-1), -2)
+        
+class TestRestar(unittest.TestCase):
+    def test_resta(self):
+        self.assertEqual(resta(3,2), 1)
+        self.assertEqual(resta(2,5), -3)
+        self.assertEqual(resta(-1,1), -2)
+        self.assertEqual(resta(1,1), 0)
+        self.assertEqual(resta(-1,-1), 0)
+        self.assertEqual(resta(1,-1), 2)
+        
+class TestMultipliar(unittest.TestCase):
+    def test_multiplicar(self):
+        self.assertEqual(multiplicacion(3,2), 6)
+        self.assertEqual(multiplicacion(5,0), 0)
+        self.assertEqual(multiplicacion(6,-2), -12)
+        self.assertEqual(multiplicacion(0,5), 0)
+
+class TestDividir(unittest.TestCase):
+    def test_dividir(self):
+        self.assertEqual(division(10,5), 2)
+        self.assertEqual(division(5,10), 0.5)
+        self.assertEqual(division(6,-2), -3)
+        self.assertEqual(division(0,2), 0)
+        self.assertEqual(division(8,0), "Error, no se puede dividir entre 0")
         
 if __name__ == "__main__":
     unittest.main()
